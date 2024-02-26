a648e220d79e0bbab0c06ea298221fd296d8b0dd
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-26 10:52:52 +0100

    Step 18

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ taskForm.addEventListener('submit', (e) => {
     } 
 
     taskData.forEach(({id, title, date, description}) => {
-        (tasksContainer.innerHTML += ``);
+        (tasksContainer.innerHTML += `
+            <div class="task" id="${id}"></div>
+        `);
     });
 })
