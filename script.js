56cf3ca6c903e510dc04c54a63e2f9666f31656a
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-03-01 09:47:17 +0100

    Step 40

--- script.js
+++ script.js
@@ -46,7 +46,9 @@ const updateTaskContainer = () => {
 }
 
 const editTask = (buttonEl) => {
-    
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
 };
 
 const deleteTask = (buttonEl) => {
