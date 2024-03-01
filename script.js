ebb842e1339b4e086df1e63f0f1d3fd8a6fc6adf
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-03-01 10:22:06 +0100

    Step 49

--- script.js
+++ script.js
@@ -114,3 +114,11 @@ taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
     addOrUpdateTask();
 });
+
+const myTaskArr = [
+    { task: "Walk the Dog", date: "22-04-2022" },
+    { task: "Read some books", date: "02-11-2023" },
+    { task: "Watch football", date: "10-08-2021" },
+];
+
+localStorage.setItem("data", myTaskArr); // Open your browser console and go to the Applications tab and select Local Storage (freeCodeCamp domain)
